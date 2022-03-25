feb91bc6cc9fef212edbc87a8957a6f90a993baa
JordanWeitz <77029445+JordanWeitz@users.noreply.github.com>
2022-03-25 14:23:15 -0600

    Worked on CSS
    Emily helped with some CSS.

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -3,53 +3,53 @@ import "./projects.css";
 import { projCardData } from "../../projCardData";
 
 function Projects() {
-	const [isActive, setIsActive] = useState(null);
+  const [isActive, setIsActive] = useState(null);
 
-	return (
-		<div className="projects">
-			<div className="project__title">
-				<h3 className="project__title__header">My Recent Work</h3>
-				<br />
-				<p className="project__title__p">
-					Here are a few of my recent creations.{" "}
-					<a href="mailto:jweitz1972@gmail.com">Email me</a> if you want to see
-					more!
-				</p>
-			</div>
-			<div className="projects__cards">
-				{projCardData.map((proj) => (
-					<div
-						className="project__card"
-						key={proj.id}
-						onMouseOver={() => setIsActive(proj.id)}
-						onMouseLeave={() => setIsActive(null)}
-					>
-						<img src={proj.img} alt="" className="project__card__img" />
-						<div
-							className={`project__card__hover ${
-								isActive === proj.id ? "" : "hidden"
-							}`}
-						>
-							<div className="project__card__wrapper">
-								<p
-									className={`project__card__text ${
-										isActive === proj.id ? "" : "hidden"
-									}`}
-								>
-									{proj.desc}
-								</p>
-								<a href={proj.link}>
-									<button className="project__card__button">
-										{proj.buttonText}
-									</button>
-								</a>
-							</div>
-						</div>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className="projects">
+      <div className="project__title">
+        <h3 className="project__title__header">My Recent Work</h3>
+        <br />
+        <p className="project__title__p">
+          Here are a few of my recent creations.{" "}
+          <a href="mailto:jweitz1972@gmail.com">Email me</a> if you want to see
+          more!
+        </p>
+      </div>
+      <div className="projects__cards">
+        {projCardData.map((proj) => (
+          <div
+            className="project__card"
+            key={proj.id}
+            onMouseOver={() => setIsActive(proj.id)}
+            onMouseLeave={() => setIsActive(null)}
+          >
+            <img src={proj.img} alt="" className="project__card__img" />
+            <div
+              className={`project__card__hover ${
+                isActive === proj.id ? "" : "hidden"
+              }`}
+            >
+              <div className="project__card__wrapper">
+                <p
+                  className={`project__card__text ${
+                    isActive === proj.id ? "" : "hidden"
+                  }`}
+                >
+                  {proj.desc}
+                </p>
+                <a href={proj.link}>
+                  <button className="project__card__button">
+                    {proj.buttonText}
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -3,94 +3,94 @@ import "./skills.css";
 import { BsCodeSquare, BsFillPeopleFill } from "react-icons/bs";
 
 function Skills() {
-	return (
-		<div className="skills">
-			<div className="skills__green__wrapper">
-				<div className="skills__green__wrapper__header">
-					<h3 className="skills__green__title">Howdy, my name is Jordan.</h3>
-					<br />
-					<br />
-					<br />
-					<p className="skills__green__p">
-						Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-						Dignissimos eaque labore libero voluptatibus explicabo totam cumque
-						quia recusandae pariatur incidunt harum exercitationem, impedit
-						deleniti, cupiditate unde at iusto iste vero qui minima doloremque?
-					</p>
-				</div>
-			</div>
-			<div className="skills__cards">
-				<div className="skills__cards__wrapper">
-					<div className="skillscard">
-						<BsCodeSquare className="skillscard__icon" />
-						<br />
-						<br />
-						<h4 className="skillscard__header">Full Stack Developer</h4>
-						<p className="skillscard__p">
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-							culpa dolore odio a nemo omnis sequi dolor, dolores libero
-							molestias?
-						</p>
-						<br />
-						<br />
-						<h5 className="skillscard__title">Languages:</h5>
-						<p className="skillscard__p">
-							<span className="skillscard__p__span">React</span>, Java, Node,
-							SQL, PL/SQL, HTML, CSS, JavaScript, JORDAN
-						</p>
-						<br />
-						<br />
-						<h5 className="skillscard__title">Dev Tools:</h5>
-						<ul className="skillscard__list">
-							<li>
-								<span className="skillscard__p__span">VSCode,</span>
-							</li>
-							<li>IntelliJ,</li>
-							<li>Postman,</li>
-							<li>MongoDB,</li>
-							<li>Git,</li>
-							<li>GitHub,</li>
-							<li>Heroku,</li>
-							<li>Firebase</li>
-						</ul>
-					</div>
-					<div className="skillscard">
-						<BsFillPeopleFill className="skillscard__icon" />
-						<br />
-						<br />
-						<h4 className="skillscard__header">Leader</h4>
-						<p className="skillscard__p">
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-							culpa dolore odio a nemo omnis sequi dolor, dolores libero
-							molestias?
-						</p>
-						<br />
-						<br />
-						<h5 className="skillscard__title">Volunteering:</h5>
-						<p className="skillscard__p">
-							<span className="skillscard__p__span">DEFEND</span> - Building
-							social media centered around accountability
-						</p>
-						<br />
-						<br />
-						<h5 className="skillscard__title">Duties Include:</h5>
-						<ul className="skillscard__list">
-							<li>
-								<span className="skillscard__p__span">Scheduling</span>
-							</li>
-							<li>Systems Analysis,</li>
-							<li>Delegation,</li>
-							<li>Communication,</li>
-							<li>Git,</li>
-							<li>GitHub,</li>
-							<li>Heroku,</li>
-							<li>Firebase</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="skills">
+      <div className="skills__green__wrapper">
+        <div className="skills__green__wrapper__header">
+          <h3 className="skills__green__title">Howdy, my name is Jordan.</h3>
+          <br />
+          <br />
+          <br />
+          <p className="skills__green__p">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Dignissimos eaque labore libero voluptatibus explicabo totam cumque
+            quia recusandae pariatur incidunt harum exercitationem, impedit
+            deleniti, cupiditate unde at iusto iste vero qui minima doloremque?
+          </p>
+        </div>
+      </div>
+      <div className="skills__cards">
+        <div className="skills__cards__wrapper">
+          <div className="skillscard">
+            <BsCodeSquare className="skillscard__icon" />
+            <br />
+            <br />
+            <h4 className="skillscard__header">Full Stack Dev</h4>
+            <p className="skillscard__p">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+              culpa dolore odio a nemo omnis sequi dolor, dolores libero
+              molestias?
+            </p>
+            <br />
+            <br />
+            <h5 className="skillscard__title">Languages:</h5>
+            <p className="skillscard__p">
+              <span className="skillscard__p__span">React</span>, Java, Node,
+              SQL, PL/SQL, HTML, CSS, JavaScript, JORDAN
+            </p>
+            <br />
+            <br />
+            <h5 className="skillscard__title">Dev Tools:</h5>
+            <ul className="skillscard__list">
+              <li>
+                <span className="skillscard__p__span">VSCode,</span>
+              </li>
+              <li>IntelliJ,</li>
+              <li>Postman,</li>
+              <li>MongoDB,</li>
+              <li>Git,</li>
+              <li>GitHub,</li>
+              <li>Heroku,</li>
+              <li>Firebase</li>
+            </ul>
+          </div>
+          <div className="skillscard">
+            <BsFillPeopleFill className="skillscard__icon" />
+            <br />
+            <br />
+            <h4 className="skillscard__header">Leader</h4>
+            <p className="skillscard__p">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+              culpa dolore odio a nemo omnis sequi dolor, dolores libero
+              molestias?
+            </p>
+            <br />
+            <br />
+            <h5 className="skillscard__title">Volunteering:</h5>
+            <p className="skillscard__p">
+              <span className="skillscard__p__span">DEFEND</span> - Building
+              social media centered around accountability
+            </p>
+            <br />
+            <br />
+            <h5 className="skillscard__title">Duties Include:</h5>
+            <ul className="skillscard__list">
+              <li>
+                <span className="skillscard__p__span">Scheduling</span>
+              </li>
+              <li>Systems Analysis,</li>
+              <li>Delegation,</li>
+              <li>Communication,</li>
+              <li>Git,</li>
+              <li>GitHub,</li>
+              <li>Heroku,</li>
+              <li>Firebase</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
