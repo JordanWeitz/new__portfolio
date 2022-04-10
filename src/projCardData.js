8b886303391083b9d885b44b8340379559dd37f4
JordanWeitz <jweitz1972@gmail.com>
2022-04-10 15:29:06 -0600

    Fixed some small things like favicon

--- src/projCardData.js
+++ src/projCardData.js
@@ -16,7 +16,7 @@ export const projCardData = [
 	{
 		id: 3,
 		desc: "COMING SOON",
-		img: "https://i.imgur.com/XqQZQZL.png",
+		img: "https://images.unsplash.com/photo-1503437313881-503a91226402?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80",
 		link: "https://github.com/JordanWeitz",
 		buttonText: "Coming Soon",
 	},
